d4f0ad65885741114f9a8feb1da4902cd8cb92ba
MiraLeaps <81037563+MirandaLeaptrott@users.noreply.github.com>
2026-03-30 11:10:00 +0200

    Finally Uploading this
    Working on fixes to the VSvillage system that I had started on back in January.

--- VSVillage/src/Entity/Pathfinding/VillagerPathNode.cs
+++ VSVillage/src/Entity/Pathfinding/VillagerPathNode.cs
@@ -1,73 +1,161 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Vintagestory.API.MathTools;
 using Vintagestory.Essentials;
 
 namespace VsVillage
 {
-    public class VillagerPathNode : IEquatable<VillagerPathNode>, IComparable<VillagerPathNode>
-    {
-        public VillagerPathNode Parent;
-        public BlockPos BlockPos;
-        public bool IsDoor = false;
-        public float Cost;
-        public PathNode ToPathNode() => new PathNode(BlockPos);
-        public Vec3d ToWaypoint() => new Vec3d(BlockPos.X + 0.5, BlockPos.Y, BlockPos.Z + 0.5);
+	// Token: 0x0200002F RID: 47
+	public class VillagerPathNode : IEquatable<VillagerPathNode>, IComparable<VillagerPathNode>
+	{
+		// Token: 0x06000141 RID: 321 RVA: 0x0000B714 File Offset: 0x00009914
+		public PathNode ToPathNode()
+		{
+			return new PathNode(this.BlockPos);
+		}
 
-        public VillagerPathNode(BlockPos blockPos, BlockPos target, bool isDoor)
-        {
-            BlockPos = blockPos;
-            Cost = blockPos.DistanceSqTo(target.X, target.Y, target.Z);
-            IsDoor = isDoor;
-        }
+		// Token: 0x06000142 RID: 322 RVA: 0x0000B734 File Offset: 0x00009934
+		public Vec3d ToWaypoint()
+		{
+			return new Vec3d((double)this.BlockPos.X + 0.5, (double)this.BlockPos.Y, (double)this.BlockPos.Z + 0.5);
+		}
 
-        public VillagerPathNode(VillagerPathNode parent, Cardinal cardinal)
-        {
-            Parent = parent;
-            BlockPos = parent.BlockPos.AddCopy(cardinal.Normali.X, cardinal.Normali.Y, cardinal.Normali.Z);
-        }
+		// Token: 0x06000143 RID: 323 RVA: 0x0000B784 File Offset: 0x00009984
+		public VillagerPathNode(BlockPos blockPos, BlockPos target, bool isDoor)
+		{
+			this.BlockPos = blockPos;
+			this.IsDoor = isDoor;
+			this.gCost = 0f;
+			this.hCost = (float)Math.Sqrt((double)blockPos.DistanceSqTo((double)target.X, (double)target.Y, (double)target.Z));
+			this.pathLength = 0;
+			this.Parent = null;
+		}
 
-        public void Init(BlockPos target, bool isDoor)
-        {
-            Cost = BlockPos.DistanceSqTo(target.X, target.Y, target.Z);
-            IsDoor = isDoor;
-        }
+		// Token: 0x06000144 RID: 324 RVA: 0x0000B7E8 File Offset: 0x000099E8
+		public VillagerPathNode(VillagerPathNode parent, Cardinal cardinal)
+		{
+			this.Parent = parent;
+			this.BlockPos = parent.BlockPos.AddCopy(cardinal.Normali.X, cardinal.Normali.Y, cardinal.Normali.Z);
+			this.pathLength = parent.pathLength + 1;
+			this.gCost = 0f;
+			this.hCost = 0f;
+			this.IsDoor = false;
+		}
 
-        public bool Equals(VillagerPathNode other)
-        {
-            return BlockPos.Equals(other?.BlockPos);
-        }
+		// Token: 0x06000145 RID: 325 RVA: 0x00002CFD File Offset: 0x00000EFD
+		public void Init(BlockPos target, bool isDoor)
+		{
+			this.hCost = (float)Math.Sqrt((double)this.BlockPos.DistanceSqTo((double)target.X, (double)target.Y, (double)target.Z));
+			this.IsDoor = isDoor;
+		}
 
-        public override bool Equals(object obj)
-        {
-            if (obj is VillagerPathNode other)
-            {
-                return Equals(other);
-            }
-            return false;
-        }
+		// Token: 0x06000146 RID: 326 RVA: 0x0000B864 File Offset: 0x00009A64
+		public bool Equals(VillagerPathNode other)
+		{
+			return this.BlockPos.Equals((other != null) ? other.BlockPos : null);
+		}
 
-        public override int GetHashCode()
-        {
-            return BlockPos.GetHashCode();
-        }
+		// Token: 0x06000147 RID: 327 RVA: 0x0000B890 File Offset: 0x00009A90
+		public override bool Equals(object obj)
+		{
+			VillagerPathNode villagerPathNode = obj as VillagerPathNode;
+			return villagerPathNode != null && this.Equals(villagerPathNode);
+		}
 
-        public List<VillagerPathNode> RetracePath()
-        {
-            var current = this;
-            var result = new List<VillagerPathNode>([current]);
-            while (current.Parent != null)
-            {
-                current = current.Parent;
-                result.Add(current);
-            }
-            result.Reverse();
-            return result;
-        }
+		// Token: 0x06000148 RID: 328 RVA: 0x0000B8B8 File Offset: 0x00009AB8
+		public override int GetHashCode()
+		{
+			return this.BlockPos.GetHashCode();
+		}
 
-        public int CompareTo(VillagerPathNode other)
-        {
-            return Cost.CompareTo(other.Cost);
-        }
-    }
+		// Token: 0x06000149 RID: 329 RVA: 0x0000B8D8 File Offset: 0x00009AD8
+		public List<VillagerPathNode> RetracePath()
+		{
+			List<VillagerPathNode> list = new List<VillagerPathNode>(this.pathLength + 1);
+			for (int i = 0; i <= this.pathLength; i++)
+			{
+				list.Add(null);
+			}
+			VillagerPathNode villagerPathNode = this;
+			for (int j = this.pathLength; j >= 0; j--)
+			{
+				list[j] = villagerPathNode;
+				villagerPathNode = villagerPathNode.Parent;
+			}
+			return list;
+		}
+
+		// Token: 0x0600014A RID: 330 RVA: 0x0000B950 File Offset: 0x00009B50
+		public int CompareTo(VillagerPathNode other)
+		{
+			int num = this.fCost.CompareTo(other.fCost);
+			bool flag = num == 0;
+			bool flag2 = flag;
+			if (flag2)
+			{
+				num = this.hCost.CompareTo(other.hCost);
+			}
+			bool flag3 = num == 0;
+			bool flag4 = flag3;
+			if (flag4)
+			{
+				num = this.BlockPos.GetHashCode().CompareTo(other.BlockPos.GetHashCode());
+			}
+			return num;
+		}
+
+		// Token: 0x0600014B RID: 331 RVA: 0x0000B9C8 File Offset: 0x00009BC8
+		public float distanceTo(VillagerPathNode other)
+		{
+			int num = Math.Abs(this.BlockPos.X - other.BlockPos.X);
+			int num2 = Math.Abs(this.BlockPos.Y - other.BlockPos.Y);
+			int num3 = Math.Abs(this.BlockPos.Z - other.BlockPos.Z);
+			int num4 = Math.Min(num, num3);
+			int num5 = Math.Max(num, num3);
+			return (float)(1.414 * (double)num4 + (double)(num5 - num4) + (double)num2);
+		}
+
+		// Token: 0x0600014C RID: 332 RVA: 0x0000BA5C File Offset: 0x00009C5C
+		public void SetGCostFromParent(VillagerPathNode parent, float extraCost)
+		{
+			bool flag = parent != null;
+			bool flag2 = flag;
+			if (flag2)
+			{
+				this.gCost = parent.gCost + parent.distanceTo(this) + extraCost;
+			}
+			else
+			{
+				this.gCost = extraCost;
+			}
+		}
+
+		// Token: 0x17000024 RID: 36
+		// (get) Token: 0x0600014D RID: 333 RVA: 0x0000BA98 File Offset: 0x00009C98
+		public float fCost
+		{
+			get
+			{
+				return this.gCost + this.hCost;
+			}
+		}
+
+		// Token: 0x040000AC RID: 172
+		public VillagerPathNode Parent;
+
+		// Token: 0x040000AD RID: 173
+		public BlockPos BlockPos;
+
+		// Token: 0x040000AE RID: 174
+		public bool IsDoor;
+
+		// Token: 0x040000AF RID: 175
+		public float gCost;
+
+		// Token: 0x040000B0 RID: 176
+		public float hCost;
+
+		// Token: 0x040000B1 RID: 177
+		public int pathLength;
+	}
 }
--- VSVillage/src/Entity/Pathfinding/VillagerPathfind.cs
+++ VSVillage/src/Entity/Pathfinding/VillagerPathfind.cs
@@ -1,49 +1,60 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
-using Vintagestory.Essentials;
 
 namespace VsVillage
 {
-    public class VillagerPathfind
-    {
-        private VillagerAStarNew villagerAStar;
-        private WaypointAStar waypointAStar;
+	// Token: 0x0200002E RID: 46
+	public class VillagerPathfind
+	{
+		// Token: 0x0600013C RID: 316 RVA: 0x0000B674 File Offset: 0x00009874
+		public VillagerPathfind(ICoreServerAPI sapi)
+		{
+			ICachingBlockAccessor cachingBlockAccessor = sapi.World.GetCachingBlockAccessor(true, true);
+			this.villagerAStar = new VillagerAStarNew(cachingBlockAccessor);
+			this.waypointAStar = new WaypointAStar(cachingBlockAccessor);
+		}
 
-        public VillagerPathfind(ICoreServerAPI sapi)
-        {
-            var blockAccessor = sapi.World.GetCachingBlockAccessor(true, true);
-            villagerAStar = new VillagerAStarNew(blockAccessor);
-            waypointAStar = new WaypointAStar(blockAccessor);
-        }
+		// Token: 0x0600013D RID: 317 RVA: 0x00002CDB File Offset: 0x00000EDB
+		public BlockPos GetStartPos(Vec3d startPos)
+		{
+			return this.villagerAStar.GetStartPos(startPos);
+		}
 
-        public BlockPos GetStartPos(Vec3d startPos)
-        {
-            return villagerAStar.GetStartPos(startPos);
-        }
+		// Token: 0x0600013E RID: 318 RVA: 0x00002CE9 File Offset: 0x00000EE9
+		public List<VillagerPathNode> FindPath(BlockPos start, BlockPos end, Village village)
+		{
+			return this.villagerAStar.FindPath(start, end, 5000);
+		}
 
-        public List<VillagerPathNode> FindPath(BlockPos start, BlockPos end, Village village)
-        {
-            var path = villagerAStar.FindPath(start, end, 5000);
-            return path;
-        }
+		// Token: 0x0600013F RID: 319 RVA: 0x0000B6B0 File Offset: 0x000098B0
+		public List<Vec3d> FindPathAsWaypoints(BlockPos start, BlockPos end, Village village)
+		{
+			List<VillagerPathNode> list = this.FindPath(start, end, village);
+			if (list != null)
+			{
+				return this.ToWaypoints(list);
+			}
+			return null;
+		}
 
-        public List<Vec3d> FindPathAsWaypoints(BlockPos start, BlockPos end, Village village)
-        {
-            List<VillagerPathNode> nodes = FindPath(start, end, village);
-            return nodes == null ? null : ToWaypoints(nodes);
-        }
-        public List<Vec3d> ToWaypoints(List<VillagerPathNode> path)
-        {
-            List<Vec3d> waypoints = new List<Vec3d>(path.Count + 1);
-            for (int i = 1; i < path.Count; i++)
-            {
-                waypoints.Add(path[i].ToWaypoint());
-            }
+		// Token: 0x06000140 RID: 320 RVA: 0x0000B6D4 File Offset: 0x000098D4
+		public List<Vec3d> ToWaypoints(List<VillagerPathNode> path)
+		{
+			List<Vec3d> list = new List<Vec3d>(path.Count + 1);
+			for (int i = 1; i < path.Count; i++)
+			{
+				list.Add(path[i].ToWaypoint());
+			}
+			return list;
+		}
 
-            return waypoints;
-        }
+		// Token: 0x040000AA RID: 170
+		private VillagerAStarNew villagerAStar;
 
-    }
+		// Token: 0x040000AB RID: 171
+		private WaypointAStar waypointAStar;
+	}
 }
